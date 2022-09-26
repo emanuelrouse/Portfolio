eb0115e4078e31d77f63b573872cb9b61dd704ee
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-09-26 02:30:59 -0400

    added new project folder and updated resume btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-// const d = document.querySelector();
-// grab the footer
-// write the year to the element
-// var d = new Date()
-// document.write(d.getFullYear())
-
-// Grab the nav and set it to active
 console.log("connected");
-
-document.querySelector;
