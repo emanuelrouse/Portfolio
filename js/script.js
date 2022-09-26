1beca0ce461bed78a886dfaf32190ed44844c5a2
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-09-25 23:28:26 -0400

    added margin to footer and changed message input into textarea

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-const d = document.querySelector();
+// const d = document.querySelector();
 // grab the footer
 // write the year to the element
 // var d = new Date()
 // document.write(d.getFullYear())
 
 // Grab the nav and set it to active
+console.log("connected");
+
+document.querySelector;
