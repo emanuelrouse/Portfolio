a0e1d817f8dde61e0656003f4e9ebcff290ffa5b
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-09-25 22:53:44 -0400

    style changes: changed bgc, h2 text color, and removed hr width

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const d = document.querySelector();
 // grab the footer
 // write the year to the element
-  // var d = new Date()
-  // document.write(d.getFullYear())
+// var d = new Date()
+// document.write(d.getFullYear())
+
+// Grab the nav and set it to active
